1ca8ee581c4488732acf2b015a1927ccb3d22630
Cono52 <oflanac52@gmail.com>
2017-05-18 10:21:04 +0100

    basic structure board board-slat-hole

--- src/App.js
+++ src/App.js
@@ -2,17 +2,62 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
+class Hole extends Component {
+  render(){
+    return (
+      <div className="Hole"></div>
+    )
+  }
+}
+
+
+
+class Slat extends Component {
+  render(){
+    return (
+      <div className="Slat">
+        <Hole></Hole>
+        <Hole></Hole>
+        <Hole></Hole>
+        <Hole></Hole>
+        <Hole></Hole>
+        <Hole></Hole>
+      </div>
+    )
+  }
+}
+
+
+class Board extends Component {
+  render(){
+    return (
+      <div className="Board">
+        <Slat></Slat>
+        <Slat></Slat>
+        <Slat></Slat>
+        <Slat></Slat>
+        <Slat></Slat>
+        <Slat></Slat>
+        <Slat></Slat>
+      </div>
+    )
+  }
+}
+
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>C - 4</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="Game">
+          <Board></Board>
+        </div>
       </div>
     );
   }
