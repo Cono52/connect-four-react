2c66399acf37cfd390fdf239ae3e49b9d274499f
Cono52 <oflanac52@gmail.com>
2017-05-18 23:55:37 +0100

    initial logic and generation of board

--- src/App.js
+++ src/App.js
@@ -15,15 +15,39 @@ class Hole extends Component {
 
 
 class Slat extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      piecesSoFar: 0,
+      holes: new Array(this.props.depth).fill(0)
+    }
+  }
+
+  dropPiece(){
+
+    const pieces = this.state.piecesSoFar;
+    const depth = this.state.holes.length;
+
+    console.log("Drop a piece into a slat")
+    if(pieces < depth ){
+      this.setState({
+        piecesSoFar: this.state.piecesSoFar+1
+      })
+    }
+    console.log(this.state.piecesSoFar)
+  }
+
+
   render(){
+
+    let holes = [...Array(this.state.holes.length)].map((x, j) => 
+      <Hole key={j}></Hole>
+    )
+
     return (
-      <div className="Slat">
-        <Hole></Hole>
-        <Hole></Hole>
-        <Hole></Hole>
-        <Hole></Hole>
-        <Hole></Hole>
-        <Hole></Hole>
+      <div className="Slat" onClick={() => this.dropPiece()}>
+        {holes}
       </div>
     )
   }
@@ -31,16 +55,27 @@ class Slat extends Component {
 
 
 class Board extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      dimensions: { rows: 6, cols: 7 }
+    };
+  }
+
+  handleClick() {
+    console.log("Clicked Board")
+  }
+
   render(){
+
+    let slats = [...Array(this.state.dimensions.cols)].map((x, j) => 
+      <Slat key={j} depth={this.state.dimensions.rows}></Slat>
+    )
+
     return (
-      <div className="Board">
-        <Slat></Slat>
-        <Slat></Slat>
-        <Slat></Slat>
-        <Slat></Slat>
-        <Slat></Slat>
-        <Slat></Slat>
-        <Slat></Slat>
+      <div className="Board" onClick={() => this.handleClick()}>
+        {slats}
       </div>
     )
   }
@@ -53,7 +88,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>C - 4</h2>
+          <h2>Griderator</h2> {/*<h2>C &#9829; J</h2>*/}
         </div>
         <div className="Game">
           <Board></Board>
